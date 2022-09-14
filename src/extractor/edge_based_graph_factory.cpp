1bb3ff5cfe5fa65a0fccca2d755c652eff61d027
JeanNaude <jean@spatialedge.co.za>
2022-09-14 17:45:28 +0200

    added more files

--- src/extractor/edge_based_graph_factory.cpp
+++ src/extractor/edge_based_graph_factory.cpp
@@ -30,8 +30,8 @@
 #include <tuple>
 #include <unordered_map>
 
-#include <blocked_range.h>
-#include <parallel_for.h>
+#include <src/extractor/blocked_range.h>
+#include <src/extractor/parallel_for.h>
 #include <src/extractor/parallel_pipeline.h>
 
 namespace std
